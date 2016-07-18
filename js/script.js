549fe526b92163d858358a369fab407cd4c17bd3
Charles Javelona <charlesjavelona@gmail.com>
2016-07-18 11:25:20 -0400

    Added images

--- js/script.js
+++ js/script.js
@@ -8,16 +8,7 @@ $(document).one('pagecreate',function() {
 	$('form').on('submit' , function(event){
 		event.preventDefault();
 		$('#mainList').append('<li>' + 
-					'<div class="whiteRec">' +
-							'<img class="checkboxImage" src="images/checkboxImage.png">' +
-							'<img class="checkImage" src="img/checkImage.png">' +
-							'<div class="item">' +
 								$('#newNote').val() +
-							'</div>' +
-							'<a href="javascript:undefined;" class="removeLink">' +
-								'<img class="removeLink" src="img/removeLink.png">' +
-							'</a>' +
-						'</div>' +
 					'</li>'
 				);
 		$('#mainList').listview('refresh');
