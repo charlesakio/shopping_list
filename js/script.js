14d483421b34d106b0bd3d10791648b684098e4d
Charles Javelona <charlesjavelona@feddev2.ddi.sheridanc.on.ca>
2016-07-18 12:46:36 -0400

    CRUD Working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 
 $(document).one('pagecreate',function() {
-	
-	$(document).on('swipeleft','li', function(){
-		$(this).remove();
-	});
 
+
+	/*Add new item*/
 	$('form').on('submit' , function(event){
 		event.preventDefault();
-		$('#mainList').append('<li>' + 
-								$('#newNote').val() +
+		$('#mainList').append('<li>' +
+					'<div class="whiteRect">' +
+						'<img class="checkboxImage" src="img/checkboxImage.png" alt="blank check box">'						+	'<img class="checkImage" src="img/checkImage.png">' + 
+						'<div class="item">' + 
+							$('#newNote').val() +
+						'</div>' +
+						'<a href="javascript:undefined;" class="removeLink">' + 
+							'<img class="removeLink" src="img/removeLink.png" alt="X image">' +
+						'</a>' + 
+					 '</div>' +
 					'</li>'
 				);
 		$('#mainList').listview('refresh');
@@ -16,4 +22,33 @@ $(document).one('pagecreate',function() {
 		$.mobile.changePage('#notesPage');
 	});
 
+	/*Delete item*/
+	$(document).on('click', '.removeLink', function(){
+		$(this).closest("li").slideUp();
+	});
+
+	/*Checkbox on*/
+	$(document).on('click', '.checkboxImage', function() {
+		$(this).closest(".whiteRect").find(".checkImage").show();
+	});
+
+	/*Checkbox off*/
+	$(document).on('click', '.checkImage', function() {
+		$(this).hide();
+	});
+
+
+	/*Edit item*/
+	$(document).on('click', '.item', function() {
+		var itemText = $(this).text();
+		$(this).text('').append($("<input class='editInput' maxlength='25' />"));
+		$(document).find(".editInput").val(itemText);
+		$("input").focus();
+	}); 
+
+	$(document).on('blur', '.editInput', function(e) {
+		if ($(this).val() !== "") {
+			$(this).parent().text($(this).val());
+		}
+	});
 });
