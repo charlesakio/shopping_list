dce6c8e488b7c48e825fbf10a93a2bf43579eb49
Charles Javelona <charlesjavelona@gmail.com>
2016-07-26 20:06:51 -0400

    Fixed localStorage

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
 
 $(document).one('pagecreate',function() {
 
+	/*Load local storage items*/
+	var items = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : items = [];
+
+	console.log(items);
+	items.forEach(function(key) {
+		appendItem(key);
+	});
 
 	/*Add new item*/
 	$('form').on('submit' , function(event){
 		event.preventDefault();
-		$('#mainList').append('<li>' +
-					'<div class="whiteRect">' +
-						'<img class="checkboxImage" src="img/checkboxImage.png" alt="blank check box">'						+	'<img class="checkImage" src="img/checkImage.png">' + 
-						'<div class="item">' + 
-							$('#newNote').val() +
-						'</div>' +
-						'<a href="javascript:undefined;" class="removeLink">' + 
-							'<img class="removeLink" src="img/removeLink.png" alt="X image">' +
-						'</a>' + 
-					 '</div>' +
-					'</li>'
-				);
+		
+		var item = $('#newNote').val();
+		
+		//Append this to the arrayItems;
+		saveToLocalStorage(item);
+
+		//Create the new item and inject to list
+		appendItem(item);
+
 		$('#mainList').listview('refresh');
-		$('#newNote').val('')
 		$.mobile.changePage('#notesPage');
 	});
 
 	/*Delete item*/
-	$(document).on('click', '.removeLink', function(){
-		$(this).closest("li").slideUp();
+	$(document).on('click', 'li', function(){
+		var item = $(this).closest('li');
+		var index = items.indexOf($(item).text());
+		items.splice(index, 1);
+		console.log(items);
+		localStorage.setItem("items", JSON.stringify(items));
+		$(item).slideUp();
 	});
 
 	/*Checkbox on*/
@@ -51,4 +59,27 @@ $(document).one('pagecreate',function() {
 			$(this).parent().text($(this).val());
 		}
 	});
+
+	/*Save data to localStorage*/
+	function saveToLocalStorage(data) {
+		items.push(data);
+		console.log(items);
+		localStorage.setItem('items', JSON.stringify(items));
+	}
+
+	/*Append item to html*/
+	function appendItem(data) {	
+		$('#mainList').append('<li class="ui-li-static ui-body-inherit ui-first-child ui-last-child">' + 
+				'<div class="whiteRect">' +
+					'<img class="checkboxImage" src="img/checkboxImage.png" alt="blank check box">'	+
+					'<img class="checkImage" src="img/checkImage.png">' +
+					'<div class="item">' +  data  + '</div>' +
+					'<a href="javascript:undefined;" class="removeLink">' + 
+						'<img class="removeLink" src="img/removeLink.png" alt="X image">' +
+						'</a>' + 
+				'</div>' +
+			'</li>'
+			);
+	
+	}//appendItem
 });
